05966f4d71a63198b1911fa8a7a76830e35123a8
saiminh <saiminh@gmail.com>
2023-03-18 17:26:58 +0100

    update header & footer templates

--- patterns/footer-default.php
+++ patterns/footer-default.php
@@ -6,23 +6,96 @@
  * Block Types: core/template-part/footer
  */
 ?>
-<!-- wp:group {"layout":{"type":"constrained"}} -->
-<div class="wp-block-group">
-	<!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|40"}}},"layout":{"type":"flex","justifyContent":"space-between"}} -->
-	<div class="wp-block-group alignwide" style="padding-top:var(--wp--preset--spacing--40)">
-		<!-- wp:site-title {"level":0} /-->
-		<!-- wp:paragraph {"align":"right"} -->
-		<p class="has-text-align-right">
-		<?php
-		printf(
-			/* Translators: WordPress link. */
-			esc_html__( 'Proudly powered by %s', 'twentytwentythree' ),
-			'<a href="' . esc_url( __( 'https://wordpress.org', 'twentytwentythree' ) ) . '" rel="nofollow">WordPress</a>'
-		)
-		?>
-		</p>
-		<!-- /wp:paragraph -->
-	</div>
-	<!-- /wp:group -->
-</div>
+<!-- wp:group {"backgroundColor":"contrast","textColor":"base","layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-base-color has-contrast-background-color has-text-color has-background"><!-- wp:separator {"align":"wide","backgroundColor":"base"} -->
+<hr class="wp-block-separator alignwide has-text-color has-base-color has-alpha-channel-opacity has-base-background-color has-background"/>
+<!-- /wp:separator -->
+
+<!-- wp:heading {"align":"wide"} -->
+<h2 class="alignwide"><a href="/contact/" data-type="page" data-id="41">Get in touch</a></h2>
+<!-- /wp:heading -->
+
+<!-- wp:separator {"align":"wide","backgroundColor":"base"} -->
+<hr class="wp-block-separator alignwide has-text-color has-base-color has-alpha-channel-opacity has-base-background-color has-background"/>
+<!-- /wp:separator -->
+
+<!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"0"}}},"className":"footer-hygiene-row","layout":{"type":"flex","justifyContent":"space-between","verticalAlignment":"top"}} -->
+<div class="wp-block-group alignwide footer-hygiene-row" style="padding-top:0"><!-- wp:group {"style":{"border":{"top":{"width":"0px","style":"none"},"right":{"width":"0px","style":"none"},"bottom":{"width":"0px","style":"none"},"left":{"width":"1px"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="border-top-style:none;border-top-width:0px;border-right-style:none;border-right-width:0px;border-bottom-style:none;border-bottom-width:0px;border-left-width:1px"><!-- wp:paragraph -->
+<p>Amsterdam</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Berlin</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Stockholm</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"border":{"left":{"width":"1px"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="border-left-width:1px"><!-- wp:paragraph -->
+<p><a href="phone:+31 85 001 4343" data-type="URL" data-id="phone:+31 85 001 4343">+31 85 001 4343</a></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><a href="mailto:info@peak.capital">info@peak.capital</a></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"border":{"left":{"width":"1px"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="border-left-width:1px"><!-- wp:paragraph -->
+<p><a href="https://medium.com/peak-capital?swcfpc=1">Medium</a></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><a href="https://twitter.com/peakcapitalvc?lang=en">Twitter</a></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><a href="https://www.linkedin.com/company/peak-capital?swcfpc=1">LinkedIn</a></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"border":{"left":{"width":"1px"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="border-left-width:1px"><!-- wp:paragraph -->
+<p><a href="/team/" data-type="page" data-id="2">Team</a></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><a href="/founders/" data-type="page" data-id="5">Founders</a></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><a href="/focus/" data-type="page" data-id="10">Focus</a></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><a href="/blog/" data-type="page" data-id="17">Thoughts &amp; Resources</a></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><a href="/contact/" data-type="page" data-id="41">Contact</a></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><a href="/jobs">Jobs</a></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"border":{"left":{"width":"1px"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="border-left-width:1px"><!-- wp:create-block/peak-logo {"url":"#wp\u002d\u002dskip-link\u002d\u002dtarget"} -->
+<div class="wp-block-create-block-peak-logo"><a href="#wp--skip-link--target"><svg class="peak-logo-svg" xmlns="http://www.w3.org/2000/svg" viewBox="-1 -1 102 102"><circle class="peak-logo-svg-circle" cx="50" cy="50" r="50"></circle><polygon class="peak-logo-svg-letters" points="70.45 39.19 70.45 61.48 75.19 61.48 75.19 53.82 78.47 50.48 84.61 61.48 90.66 61.48 82.14 46.79 89.65 39.19 83.69 39.19 75.19 47.68 75.19 39.19 70.45 39.19"></polygon><polygon class="peak-logo-svg-letters" points="43.83 43.41 43.83 39.22 28.98 39.22 28.98 61.48 43.83 61.48 43.83 57.29 33.71 57.29 33.71 52.23 43.2 52.23 43.2 48.03 33.71 48.03 33.71 43.41 43.83 43.41"></polygon><path class="peak-logo-svg-letters" d="M45.77,61.48h5.12L51.48,60a3,3,0,0,1,.16-.36,6,6,0,0,1,11-.21,2.92,2.92,0,0,1,.14.31l.7,1.78h5.15l-8.9-22.26h-5Zm13.95-9.63a10.56,10.56,0,0,0-2.53-.31,9.85,9.85,0,0,0-2.55.33l2.56-6.52Z"></path><path class="peak-logo-svg-letters" d="M9.34,39.22V61.48h4.73V53.57h3.4c5.56,0,9-2.71,9-7.18s-3.46-7.17-9-7.17Zm12.29,7.17c0,1.91-1.48,3-4.16,3h-3.4v-6h3.4C20.15,43.41,21.63,44.49,21.63,46.39Z"></path></svg></a></div>
+<!-- /wp:create-block/peak-logo -->
+
+<!-- wp:paragraph -->
+<p>© Peak 2022</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:separator {"align":"wide","backgroundColor":"base"} -->
+<hr class="wp-block-separator alignwide has-text-color has-base-color has-alpha-channel-opacity has-base-background-color has-background"/>
+<!-- /wp:separator --></div>
 <!-- /wp:group -->
