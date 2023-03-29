dd4ff93f591b06a69864b30eef729cb90094266d
saiminh <saiminh@gmail.com>
2023-03-29 14:59:36 +0200

    update font license, faq, filter and search ui

--- assets/js/cat-filter.js
+++ assets/js/cat-filter.js
@@ -1,7 +1,31 @@
 document.addEventListener('DOMContentLoaded', function() {
   const catfilter = document.querySelector('.peak-category-filter');
-  catfilter && catfilter.querySelector('a[href="#categories"]').addEventListener('click', function(e) {
+  let cats = catfilter.querySelector('#categories');
+  cats.style.display = 'block';
+  let origHeight = cats.offsetHeight + 'px';
+  cats.style.height = '0px';
+
+  function toggleCatFilter(e) {
     e.preventDefault();
-    catfilter.classList.toggle('expanded');
+    let isOpen = catfilter.classList.contains('expanded');
+    isOpen ? catfilter.classList.remove('expanded') : catfilter.classList.add('expanded');
+    cats.style.height = isOpen ? '0px' : origHeight;
+  }
+  catfilter && catfilter.querySelector('a[href="#categories"]').addEventListener('click', function(e) {
+    toggleCatFilter(e);
+  })
+  catfilter && catfilter.addEventListener('mouseenter', function(e) {
+    toggleCatFilter(e);
+  })
+  catfilter && catfilter.addEventListener('mouseleave', function(e) {
+    toggleCatFilter(e);
+  })
+
+  const searchField = document.querySelector('.wp-block-search__input');
+  searchField && searchField.addEventListener('focusin', function(e) {
+    searchField.closest('form').classList.add('search-focused');
+  })
+  searchField && searchField.addEventListener('focusout', function(e) {
+    searchField.closest('form').classList.remove('search-focused');
   })
 })
--- assets/js/faqs.js
+++ assets/js/faqs.js
@@ -1,14 +1,15 @@
 window.onload = function() {
   document.querySelectorAll('.peak-faq').forEach(faq => {
+    const q = faq.querySelector('.peak-faq-answer');
+    q.style.display = 'block';
+    let origHeight = q.offsetHeight + 'px';
+    q.style.height = '0px';
+    
     faq.querySelector('.peak-faq-question').addEventListener('click', function(e) {
       e.preventDefault();
       let isOpen = faq.classList.contains('peak-faq-open');
-      document.querySelector('.peak-faq-open') && document.querySelector('.peak-faq-open').classList.remove('peak-faq-open');
       isOpen ? faq.classList.remove('peak-faq-open') : faq.classList.add('peak-faq-open');
-      window.scrollTo({
-        top: faq.offsetTop - 50,
-        behavior: 'smooth'
-      });
+      q.style.height = isOpen ? '0px' : origHeight;
     })
   })
 }
